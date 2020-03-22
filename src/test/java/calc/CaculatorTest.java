957c419b9a9184a8e4356d4fc8983bc26458c280
Song. <zjs849437698@163.com>
2020-03-22 23:22:37 +0100

    testing

---
+++ src/test/java/calc/CaculatorTest.java
@@ -0,0 +1,53 @@
+package calc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CaculatorTest {
+	private static CalculatorMain calc;
+	
+	@BeforeClass
+	public static void setup(){
+		calc = new CalculatorMain();
+	}
+	
+	@Test
+	public void testAdd() {
+		int a=6;
+		int b=4;
+		int result=10;
+
+		Assert.assertEquals(result, calc.add(a, b));
+	}
+
+	@Test
+	public void testSubtract() {
+		int a=16;
+		int b=4;
+		int result=12;
+		
+		Assert.assertEquals(result, calc.sub(a, b));
+	}
+
+	@Test
+	public void testMultiply() {
+		int a=6;
+		int b=4;
+		int result=24;
+		
+		Assert.assertEquals(result, calc.mul(a, b));
+	}
+
+	@Test
+	public void testDivide() {
+		int a=16;
+		int b=2;
+		int result=8;
+		
+		Assert.assertEquals(result, calc.div(a, b));
+	}
+	
+}
